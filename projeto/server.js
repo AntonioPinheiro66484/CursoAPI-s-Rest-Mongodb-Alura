b55ebcdd6ab6cbb11e4852430f6eb8a88c72f932
HadeSatanist <ap66484@gmail.com>
2023-03-05 11:12:42 -0300

    Implementação de metodos put, get, delete, post no express

--- projeto/server.js
+++ projeto/server.js
@@ -1,19 +1,6 @@
-const http = require("http")
-const port = 3000;
+import app from "./src/app.js"
+const port = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros':'Entrei na pagina de Livros',
-    '/autores':'Listagem de autores',
-    '/editora':'Pagina Editora',
-    '/sobre':'Informação sobre o projeto',
-}
-
-const server = http.createServer((req,res) =>{
-    res.writeHead(200,{"Content-Type" : "text/plain"});
-    res.end(rotas[req.url]);
-})
-
-server.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log(`Servidor escatando em http://localhost:${port}`)
 })
---
+++ projeto/src/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+const livros = [ 
+    {id: 1, "titulo": "Senhor dos Aneis"},
+    {id: 2, "titulo": "O Hobiit"}
+]
+
+app.get('/', (req, res)=>{
+    res.status(200).send('Curso de Node');
+})
+
+app.get('/livros', (req, res)=>{
+    res.status(200).json(livros);
+})
+
+app.get('/livros/:id',(req,res)=>{
+    let index = buscalivro(req.params.id)
+    res.json(livros[index]);
+})
+
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('O livro foi cadastrado com sucesso')
+})
+
+app.put('/livros/:id',(req,res)=>{
+    let index = buscalivro(req.params.id)
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+app.delete('/livros/:id',(req,res)=>{
+    let {id} = req.params;
+    let index = buscalivro(id);
+    livros.splice(index, 1);
+    res.send(`Livro ${id} removido com sucesso`);
+    res.json(livros);
+})
+
+function buscalivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
+export default app
