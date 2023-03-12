c0a189e7c2256b432ac1ee90e3409a6455536d3b
HadeSatanist <ap66484@gmail.com>
2023-03-12 19:40:42 -0300

    removendo o impor de Livros que não estava sendo mais utilizado

--- projeto/src/app.js
+++ projeto/src/app.js
@@ -1,27 +1,14 @@
 import express from 'express';
 import db from './config/dbConnect.js'
-import livros from './models/Livro.js';
 import routes from './routes/index.js';
 
 db.on("error", console.log.bind(console,'Erro de conexão'))
 db.once("open",  ()=>{
     console.log("conexão com o banco feita com sucesso")
 })
+
 const app = express();
-
 app.use(express.json())
-
 routes(app);
 
-app.delete('/livros/:id',(req,res)=>{
-    let {id} = req.params;
-    let index = buscalivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso`);
-    res.json(livros);
-})
-
-function buscalivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
 export default app
